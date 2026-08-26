f5f89c5fc683efd1cc6c861ffa38d6329d0d9507
tanslaptop876-oss <tanslaptop876@gmail.com>
2026-08-26 10:05:04 +0530

    Integrate verified Studio Salvador profile and safe connectors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='marketingmind-v47';
+const CACHE='marketingmind-v49';
 const SHELL=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
@@ -6,4 +6,3 @@ self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
   event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(match=>match||caches.match('/index.html'))));
 });
-
